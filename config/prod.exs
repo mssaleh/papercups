8b1b8cabfc91561bd83891a477fb894a702791ae
Mohammed Saleh <mssaleh@users.noreply.github.com>
2021-11-16 09:13:17 +0400

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -21,4 +21,5 @@ config :chat_api, ChatApi.Repo,
   socket_options: [:inet],
   pool_size: pool_size
 
-config :chat_api, ChatApiWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+# config :chat_api, ChatApiWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+config :chat_api, ChatApiWeb.Endpoint
